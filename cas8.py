4992de24d5b57bf12f30d872f924d6b5d78b3eb2
Yugandran1812 <46665588+Yugandran1812@users.noreply.github.com>
2019-02-01 09:04:33 +0530

    Update cas8.py

--- cas8.py
+++ cas8.py
@@ -1,10 +1,6 @@
-def main():
-    a=int(input(""))
-    b = 0 
-    while(a > 0):
-        b += a
-        a -= 1
-        print(b)
-if __name__ == '__main__':
 
-    main()
+m=0
+n=int(input())
+for i in range(0,n+1):
+	m=m+i
+print(m)
