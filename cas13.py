ee4be5423f3f50d77a7b912bd557e35a9c730611
Yugandran1812 <46665588+Yugandran1812@users.noreply.github.com>
2019-02-04 14:02:25 +0530

    Create cas13.py

---
+++ cas13.py
@@ -0,0 +1,11 @@
+num=int(input())
+if num>0:
+    for i in range(2,num):
+        if (num%i==0):
+            print ("no")
+            break
+        else:
+            print("yes")
+            break
+else:
+    print("no")
