3d757815497eaaf608ba328db03800c3bf66b288
Yugandran1812 <46665588+Yugandran1812@users.noreply.github.com>
2019-02-04 13:50:44 +0530

    Update cas12.py

--- cas12.py
+++ cas12.py
@@ -1,11 +1,11 @@
-num=input()
+num=int(input())
 temp=num
 rev=0
 while(num>0):
     dig=num%10
     rev=rev*10+dig
     num=num//10
-if temp==num:
+if temp==rev:
     print("yes")
 else:
     print("no")    
